a989332d74999a8730159a5b710c3f5f2acdc804
yaekobbundure <jackmanbonke@gmail.com>
2022-06-30 11:47:49 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,11 @@
 #include <stdio.h> 
 
-int main(void) { int a; long int b; long long int c; char d; float f;
+int main(void)
+
+{
+       
+
+	int a; long int b; long long int c; char d; float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
